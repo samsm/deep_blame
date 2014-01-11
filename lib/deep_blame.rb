6656df3ce378c89ed9999f385e1d990413e97465
Sam Schenkman-Moore <samsm@samsm.com>
2014-01-11 11:31:00 -0500

    Does something.
    Multi-line commit.

--- lib/deep_blame.rb
+++ lib/deep_blame.rb
@@ -1,5 +1,10 @@
-require "deep_blame/version"
+require_relative "deep_blame/version"
 
 module DeepBlame
-  # Your code goes here...
+  def self.line_history(path, start_line, end_line = nil, ref = 'HEAD')
+    end_line ||= start_line
+    command = "git blame -p -L #{start_line},#{end_line} #{path}"
+    puts command
+    system command
+  end
 end
