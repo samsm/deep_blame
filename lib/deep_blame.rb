4564d8850edd7c7785b645ddb1a903f33a9d2828
Sam Schenkman-Moore <samsm@samsm.com>
2014-01-12 10:26:13 -0500

    This might work.

--- lib/deep_blame.rb
+++ lib/deep_blame.rb
@@ -2,18 +2,26 @@ require 'pry'
 require_relative "deep_blame/version"
 
 module DeepBlame
-  def self.line_history(path, start_line, end_line = nil, ref = 'HEAD')
-    end_line ||= start_line
-    command = "git blame -b --incremental -l -L #{start_line},#{end_line} #{path}"
-    puts command
-    result = `#{command}`
-    bims = BlameIncrementalMessage.divide(result)
-    puts "Relevant hashes: #{bims.collect(&:sha).join(", ")}."
+  def self.line_history(path, start_line, end_line = nil, rev = 'HEAD')
+    blames = Blame.find(rev, path, start_line, end_line)
+    blames.each {|blame| display(blame) }
   end
 
-  class BlameIncrementalMessage
-    def self.divide(incremental_blame_output)
-      segments = incremental_blame_output.scan(/[\s\S]+?filename.+/)
+  def self.display(blame)
+    print "Blame shas: #{blame.short_sha}"
+    while (blame = blame.parent) do
+      print " -> #{blame.short_sha}"
+    end
+    puts "."
+  end
+
+  class Blame
+    def self.find(rev, path, start_line, end_line = nil)
+      end_line ||= start_line
+      command = "git blame -b --incremental -l -L #{start_line},#{end_line} #{rev} #{path}"
+      # puts "Running: #{command}"
+      result = `#{command}`
+      segments = result.scan(/[\s\S]+?filename.+/)
       segments.collect do |segment|
         new parse_segment(segment)
       end
@@ -21,18 +29,15 @@ module DeepBlame
 
     def self.parse_segment(blame)
       first_line = blame.slice!(/.+/)
-      hsh = {}
-      hsh[:sha] = first_line.slice!(/\w+/)
-      hsh[:line_number_original_file] = first_line.slice!(/\w+/).to_i
-      hsh[:line_number_final_file]    = first_line.slice!(/\w+/).to_i
-      hsh[:line_number_group]         = first_line.slice!(/\w+/).to_i
+      hsh = [:sha, :line_number_original_file, :line_number_final_file,
+       :line_number_group].inject({}) do |h, attr|
+        h[attr] = first_line.slice!(/\w+/) ; h
+      end
       hsh[:line_number_group] = nil if hsh[:line_number_group] == 0
       blame.strip.split("\n").inject(hsh) do |h, line|
         key = line.slice!(/\S+/)
-        key.gsub!("-","_")
-        key = key.to_sym
-        h[key] = line.strip
-        h[key] = true if h[key].empty?
+        key = key.gsub("-","_").to_sym
+        h[key] = line.strip.empty? ? true : line.strip
         h
       end
     end
@@ -49,6 +54,27 @@ module DeepBlame
        define_method attr do
          data[attr]
        end
-     end
+    end
+
+    def short_sha
+      sha[0..6]
+    end
+
+    def previous_sha
+      previous[/^\S+/]
+    end
+
+    def previous_file
+      previous.sub(/^\S+\s+/,'')
+    end
+
+    def previous_line_number
+      line_number_original_file
+    end
+
+    # rev, path, start_line, end_line = nil
+    def parent
+      self.class.find(previous_sha, previous_file, previous_line_number).first unless boundary
+    end
   end
 end
